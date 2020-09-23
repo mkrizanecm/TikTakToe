3072fed2f1933a9cdc10ca8c2319724810dfd062
Matej Križanec <matej.krizanec97@gmail.com>
2020-09-23 20:05:33 +0200

    update

--- js/functions.js
+++ js/functions.js
@@ -4,13 +4,15 @@ function setImage (element) {
 }
 
 function checkSet () {
-    var first_block = document.getElementById('first');
-    var second_block = document.getElementById('second');
-    var third_block = document.getElementById('third');
-    var fourth_block = document.getElementById('fourth');
-    var fifth_block = document.getElementById('fifth');
-    var sixth_block = document.getElementById('sixth');
-    var seventh_block = document.getElementById('seventh');
-    var eight_block = document.getElementById('eight');
-    var ninth_block = document.getElementById('ninth');
+    var first_block = document.getElementById('first').innerHTML;
+    var second_block = document.getElementById('second').innerHTML;
+    var third_block = document.getElementById('third').innerHTML;
+    var fourth_block = document.getElementById('fourth').innerHTML;
+    var fifth_block = document.getElementById('fifth').innerHTML;
+    var sixth_block = document.getElementById('sixth').innerHTML;
+    var seventh_block = document.getElementById('seventh').innerHTML;
+    var eight_block = document.getElementById('eight').innerHTML;
+    var ninth_block = document.getElementById('ninth').innerHTML;
+
+    console.log(ninth_block); return;
 }
