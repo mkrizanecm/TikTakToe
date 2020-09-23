4ab06401a63275975804f9386438ad370c5191ed
Matej Križanec <matej.krizanec97@gmail.com>
2020-09-23 20:21:01 +0200

    JS update

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,9 @@
 function setImage (element) {
-    checkSet();
-    document.getElementById(element).innerHTML = "<img class='image' src='icons/x.png'/>";
+    var image = document.getElementById(element).innerHTML;
+    if (image == '') {
+        document.getElementById(element).innerHTML = "<img class='image' src='icons/o.png'/>";
+        checkSet();
+    }
 }
 
 function checkSet () {
@@ -13,6 +16,42 @@ function checkSet () {
     var seventh_block = document.getElementById('seventh').innerHTML;
     var eight_block = document.getElementById('eight').innerHTML;
     var ninth_block = document.getElementById('ninth').innerHTML;
+
+    if ((first_block == second_block && first_block == third_block) && (first_block != '' && second_block != '' && third_block != '')) {
+        console.log(1); return;
+    } 
+
+    if ((first_block == fourth_block && first_block == seventh_block) && (first_block != '' && fourth_block != '' && seventh_block != '')) {
+        console.log(1); return;
+    } 
+
+    if ((first_block == fifth_block && first_block == ninth_block) && (first_block != '' && fifth_block != '' && ninth_block != '')) {
+        console.log(1); return;
+    } 
+    
+    if ((second_block == fifth_block && second_block == eight_block) && (second_block != '' && fifth_block != '' && eight_block != '')) {
+        console.log(1); return;
+    } 
+
+    if ((second_block == second_block && first_block == third_block) && (first_block != '' && second_block != '' && third_block != '')) {
+        console.log(1); return;
+    } 
+
+    if ((third_block == fifth_block && third_block == seventh_block) && (third_block != '' && fifth_block != '' && seventh_block != '')) {
+        console.log(1); return;
+    } 
+
+    if ((third_block == sixth_block && third_block == ninth_block) && (third_block != '' && sixth_block != '' && ninth_block != '')) {
+        console.log(1); return;
+    } 
+
+    if ((fourth_block == fifth_block && fourth_block == sixth_block) && (fourth_block != '' && fifth_block != '' && sixth_block != '')) {
+        console.log(1); return;
+    } 
+
+    if ((seventh_block == eight_block && seventh_block == ninth_block) && (seventh_block != '' && eight_block != '' && ninth_block != '')) {
+        console.log(1); return;
+    } 
 
     console.log(ninth_block); return;
 }
