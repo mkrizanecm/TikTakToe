14af03f5400c7893f6f411c36d6712bab167b4da
Matej Križanec <matej.krizanec97@gmail.com>
2020-09-23 19:53:18 +0200

    Deploy to GitHub

---
+++ js/functions.js
@@ -0,0 +1,16 @@
+function setImage (element) {
+    checkSet();
+    document.getElementById(element).innerHTML = "<img class='image' src='icons/x.png'/>";
+}
+
+function checkSet () {
+    var first_block = document.getElementById('first');
+    var second_block = document.getElementById('second');
+    var third_block = document.getElementById('third');
+    var fourth_block = document.getElementById('fourth');
+    var fifth_block = document.getElementById('fifth');
+    var sixth_block = document.getElementById('sixth');
+    var seventh_block = document.getElementById('seventh');
+    var eight_block = document.getElementById('eight');
+    var ninth_block = document.getElementById('ninth');
+}
